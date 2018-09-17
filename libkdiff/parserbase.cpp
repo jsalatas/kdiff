44035783570afa8f5fbe730651f1119da1a3f40e
John Salatas <jsalatas@gmail.com>
2018-09-16 17:34:47 -0700

    accept empty lines in header

--- libkdiff/parserbase.cpp
+++ libkdiff/parserbase.cpp
@@ -562,6 +562,7 @@ void ParserBase::checkHeader(const QRegExp& header)
 {
     if(m_diffIterator != m_diffLines.end()
        && !header.exactMatch(*m_diffIterator)
+       && !m_diffIterator->startsWith("\n") /* Accept empty lines */
        && !m_diffIterator->startsWith("Index: ") /* SVN diff */
        && !m_diffIterator->startsWith("diff ") /* concatenated diff */
        && !m_diffIterator->startsWith("-- ") /* git format-patch */) {
